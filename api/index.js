4636eb50e6a9b8a654791a4bc86195ca2e7f8dc3
mudasir <mudasirbhat602@gmail.com>
2024-02-19 21:41:58 +0530

    create a text api route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,27 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
+const app = express();
 
 dotenv.config();
-// console.log("MONGO",process.env.MONGO)
+
 // console.log(process.env);
-mongoose.connect(process.env.MONGO)
+mongoose
+  .connect(process.env.MONGO)
   .then(() => {
     console.log("MongoDB connected");
-    const app = express();
-
-    const port = 3000;
-    app.listen(port, () => {
-      console.log("Server is running on port", port);
-    });
   })
-  
-  
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
+
+app.use("/api/user",userRoutes)
